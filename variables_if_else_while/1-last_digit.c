c51df7a924aecb4a85ca6f7d378504be60181e3d
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-20 11:21:32 +0000

    fix: string with better syntaxe

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,11 +11,12 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n % 10 > 5)
-printf("Last digit of %d and is greater than 5\n", n);
-else if (n % 10 == 0)
-printf("Last digit of %d and is 0\n", n);
+int last_digit = n % 10;
+if (last_digit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+else if (last_digit % 10 == 0)
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 else
-printf("Last digit of %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 return (0);
 }
