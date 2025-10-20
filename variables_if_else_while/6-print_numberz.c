046811b68c4c45410583e7937be45192bb2acbca
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-20 12:50:28 +0000

    feat: program that prints all single digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char number = 48;
+while (number < 58)
+{
+putchar(number);
+number++;
+}
+printf("\n");
+return (0);
+}
+
