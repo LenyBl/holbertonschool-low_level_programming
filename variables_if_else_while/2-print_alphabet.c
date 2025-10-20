0f91b89822da71a4e054149ffb2a3f2f0431a829
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-20 11:32:11 +0000

    feat: program to list char alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 65
+while (ch != ch + 25)
+{
+putchar(ch);
+ch++
+}
+return (0);
+}
