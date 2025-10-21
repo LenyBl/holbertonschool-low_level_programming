40dc0feecd0a10b33acfc9829e58de29c202a26b
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 09:04:09 +0000

    feat:  function that prints the last digit of a number.

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int print_last_digit(int n)
+{
+int last_digit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
+if (last_digit > 5)
+_putchar("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+else if (last_digit == 0)
+_putchar("Last digit of %d is %d and is 0\n", n, last_digit);
+else
+_putchar("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+return (0);
+}
