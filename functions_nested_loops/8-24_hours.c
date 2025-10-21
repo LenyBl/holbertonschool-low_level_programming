6a03affb4bfcae2da95a8c21897f77ef097f75ac
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 12:24:02 +0000

    fix: while boucle for minute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,29 +1,30 @@
 #include "main.h"
+
 /**
  * jack_bauer - Affiche chaque minute de la journée de Jack Bauer
  *
- * Description: Cette fonction affiche toutes les heures et minutes
- * d'une journée, en partant de 00:00 jusqu'à 23:59, chaque horaire
- * sur une nouvelle ligne.
+ * Description: Utilise des boucles while pour afficher toutes les heures
+ * et minutes de 00:00 à 23:59, chaque horaire sur une nouvelle ligne.
  *
  * Return: rien
  */
 void jack_bauer(void)
 {
 int hour = 0;
-int minute = 0;
-while (hour < 24 && minute < 60)
+int minute;
+while (hour < 24)
+{
+minute = 0;
+while (minute < 60)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
-minute++;
-if (minute == 59)
-{
-hour++;
-}
 _putchar('\n');
+minute++;
+}
+hour++;
 }
 }
