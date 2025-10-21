d4b302b1b1dc124ae1dbd73da517fb8b60c46d84
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 09:05:58 +0000

    fix: remove stdio library

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 int print_last_digit(int n)
 {
 int last_digit;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 if (last_digit > 5)
 _putchar("Last digit of %d is %d and is greater than 5\n", n, last_digit);
