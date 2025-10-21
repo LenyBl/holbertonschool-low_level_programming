9db7d20723aa8cfccca243deeed0a0b270d54f6a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 13:02:43 +0000

    feat: function that prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int print_to_98(int n)
+{
+
+if (n > 98)
+{
+while (n > 98)
+{
+_putchar((n / 10) + '0');
+_putchar(',');
+_putchar(' ');
+n--;
+}
+}
+else
+{
+while (n < 98)
+{
+_putchar((n / 10) + '0');
+_putchar(',');
+_putchar(' ');
+n++;
+}
+}
+_putchar('\n');
+}
