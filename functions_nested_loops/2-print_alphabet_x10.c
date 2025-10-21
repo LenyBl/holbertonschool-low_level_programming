ebb0df4bac1306ebdf3b28ce5ab39b5272db0851
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 07:45:12 +0000

    feat:  function that prints 10 times the alphabet, in lowercase

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - affiche l'alphabet en minuscules x10
+ *
+ * Return: rien
+ */
+void print_alphabet_x10(void)
+{
+int n = 0;
+char ch = 97;
+while (n <= 10)
+{
+while (ch <= 122)
+{
+_putchar(ch);
+ch++
+}
+_putchar('\n');
+n++;
+}
+return (0);
+}
