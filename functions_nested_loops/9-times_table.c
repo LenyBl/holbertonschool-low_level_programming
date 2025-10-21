5459bafd71c8771bcf3c04e3726358afa35357e2
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 12:50:57 +0000

    fix: structure of code

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,29 +2,37 @@
 /**
  * times_table - Affiche la table de multiplication de 9
  *
- * Description: Cette fonction affiche la table de 9
- * en format 10x10, de 0 à 9, avec les bons espacements.
+ * Description: Cette fonction affiche la table de multiplication
+ * de 9 en format 10x10, de 0 à 9, avec les bons espacements.
  *
  * Return: rien
  */
 void times_table(void)
 {
-int times = 0;
 int number = 0;
-int multiple = 0;
-while (times < 10)
+int multiple;
+int resultat;
+while (number < 10)
 {
+multiple = 0;
 while (multiple < 10)
 {
-int resultat = number * multiple;
-multiple++;
-_putchar((resultat / 10) + '0');
+resultat = number * multiple;
+if (multiple == 0)
+_putchar(resultat + '0');
+else
+{
 _putchar(',');
 _putchar(' ');
+if (resultat < 10)
+_putchar(' ');
+else
+_putchar((resultat / 10) + '0');
+_putchar((resultat % 10) + '0');
 }
-number++;
-times++;
+multiple++;
+}
 _putchar('\n');
-}
+number++;
 }
 }
