8d1a79c5810848e484771cfb9549edd5405a2aa3
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-21 12:16:10 +0000

    feat: function that prints every minute of the day of Jack Bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - Affiche chaque minute de la journée de Jack Bauer
+ *
+ * Description: Cette fonction affiche toutes les heures et minutes
+ * d'une journée, en partant de 00:00 jusqu'à 23:59, chaque horaire
+ * sur une nouvelle ligne.
+ *
+ * Return: rien
+ */
+void jack_bauer(void)
+{
+int hours = 0;
+int minutes = 0;
+
+while (hours == 24 && minutes == 60)
+{
+_putchar(hours);
+_putchar(':');
+_putchar(minutes);
+hours++;
+minutes++;
+_putchar('\n');
+}
+}
