755c222b005af19ebb94da729a08d5f74f0c1d60
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 09:13:25 +0000

    function that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "../functions_nested_loops/main.h"
+/**
+ * print_most_numbers - affiche les chiffres sauf 2 et 4
+ *
+ * Return: Rien
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -9,4 +9,6 @@ int mul(int, int);
 
 void print_numbers(void);
 
+void print_most_numbers(void);
+
 #endif
