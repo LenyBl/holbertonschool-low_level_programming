197046821baca57258fe8ba830281576ac575cdb
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 12:05:47 +0000

    feat: program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - affiche les nombres de 1 à 100 en remplaçant :
+ *        multiples de 3 par "Fizz", de 5 par "Buzz" et de 15 par "FizzBuzz"
+ *
+ * Return: Retourne 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ")
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ")
+			}
+			else
+			{
+				printf("Buzz")
+			}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+
+	return (0);
+}
