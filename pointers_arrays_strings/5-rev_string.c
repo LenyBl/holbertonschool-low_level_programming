2002a0447b3631242732edbabecdc96c2dcd1dde
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 14:23:34 +0000

    feat: function that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Envoie ne mémoire la chaine inversée
+ *
+ * @s: Chaine de caractère
+ *
+ * Return: Retourne rien
+ */
+void rev_string(char *s)
+{
+	int c;
+	int i;
+	char temp[0];
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+
+	for (i = c - 1; i >= 0; i--)
+	{
+		temp[] = s[i];
+	}
+
+}
