d49a4876989259341609fdfb9e50b42f2200b0b3
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 08:36:40 +0000

    function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplie deux entiers
+ * @a: premier entier
+ * @b: deuxième entier
+ *
+ * Return: le résultat de la multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
