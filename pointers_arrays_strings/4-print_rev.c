7a1f46c059d1081e106bec95fe1f082b9a4e2eef
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 13:57:55 +0000

    fix: rechange for another idea logique

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
  * print_rev - affiche une chaîne de caractères en ordre inverse
- *
  * @s: pointeur vers la chaîne à afficher
  *
  * Return: void
  */
 void print_rev(char *s)
 {
+	int c;
 	int i;
-	int length = _strlen(s);
 
-	for (i = length - 1; i >= 0; i--)
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+
+	for (i = c - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
