aa463b4d1e11f0390cc6fa0edc6e508f23eb7ff2
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 13:47:27 +0000

    feat:  function that prints a string, in reverse, followed by a new line

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Afficher une chaine de caractère inversé
+ *
+ * @s: Chaine de caractère
+ *
+ * Return: Retourne rien
+ */
+void print_rev(char *s)
+{
+	int num = _strlen(s);
+
+	for (i = num; i <= 0; i--)
+	{
+		_puts(s[i]);
+	}
+}
