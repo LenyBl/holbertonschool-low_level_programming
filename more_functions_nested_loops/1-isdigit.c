3a9a924e0cffd51ac5e8d2eeec5ed972b8e26497
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 08:29:33 +0000

    feat: function that checks for a digit (0 through 9)

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - vérifie si l'entier est un chiffre
+ * @i: l'entier à vérifier
+ *
+ * Return: La valeur 1 si c'est un chiffre sinon 0
+ */
+int _isdigit(int i)
+{
+	if (i >= 0 && i <= 9)
+	{
+		return (1);
+	}
+
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,5 +3,7 @@
 
 int _isupper(int);
 
+int _isdigit(int);
+
 
 #endif
