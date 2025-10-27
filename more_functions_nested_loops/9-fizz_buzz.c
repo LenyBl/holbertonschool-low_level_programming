aa5cae354bf5f9a34f9d0ea7a4be8f372396cb59
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-27 12:13:54 +0000

    fix: add else if where i = 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,12 +21,12 @@ int main(void)
 			{
 				printf("Fizz ");
 			}
+			else if (i == 100)
+			{
+				printf("Buzz");
+			}
 			else
 			{
-				if (i == 100)
-				{
-					printf("Buzz");
-				}
 				printf("Buzz ");
 			}
 		}
