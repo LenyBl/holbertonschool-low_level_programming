ea402f7ba5cd1222f690522b3899d47e51c03832
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-28 09:51:16 +0000

    fix: reform the program

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,9 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * print_array - Fonction qui affiche le nombre de caractère d'une liste
- * avec un nombre de caractère donné
- *
- * @a: Chaine de caractère
- * @n: Nombre de caractère à afficher
- *
- * Return: Rien
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the array
+ * @n: number of elements to be printed
  */
 void print_array(int *a, int n)
 {
@@ -16,12 +12,10 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-
 		if (i != n - 1)
 		{
 			printf(", ");
 		}
 	}
-
-	_putchar('\n');
+	printf("\n");
 }
