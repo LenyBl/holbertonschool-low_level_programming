4776ddd184f37460f3db9cda5e47af3c59184b52
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-28 13:27:06 +0000

    fix: corriger l'initialisation des variables i et c dans la fonction _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,10 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int c;
 
-	for (c; dest[c]; c++)
+	for (c = 0; dest[c]; c++)
 		;
 
-	for (i; src[i] != '\0'; c++)
+	for (i = 0; src[i] != '\0'; c++)
 	{
 		dest[c] = src[i];
 		i++;
