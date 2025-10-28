92151e52466414c1b417d065ea23fec2d0fc3a55
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-28 08:22:18 +0000

    fix: change value of pointer

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,17 @@
 void puts2(char *str)
 {
 	int i;
-	str = str;
 
-	for (i = 0; str[i] != '\0' ; i += 2)
+	char *new_str = malloc(strlen(str) + 1);
+
+	if (new_str == NULL)
+	{
+		return;
+	}
+
+	strcpy(new_str, str);
+
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
 	}
