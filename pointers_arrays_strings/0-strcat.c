009502000d1ef19e2ac366eb3de0791b043772cf
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-28 13:30:39 +0000

    fix: améliorer la documentation de la fonction _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
+ * *_strcat - Fonction qui permet de concaténer 2 chaines
  *
+ * @dest: Chaine de caractère
+ * @src: Chaine de caractère
  *
- *
- *
- *
+ * Return: Les deux chaines concaténée
  */
 char *_strcat(char *dest, char *src)
 {
