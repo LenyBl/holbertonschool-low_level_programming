a70f382d4d84144006ca13b4cb998e3eecabaebf
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-28 13:21:49 +0000

    feat: ajouter la fonction _strcat pour concaténer deux chaînes

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char _strcat(char *dest, char *src)
+{
+	int i = 0;
+	int c = 0;
+
+	for (c; dest[c]; c++)
+		;
+
+	for ( i ; src[i] != '\0'; i++)
+	{
+		dest[c] = src[i];
+		c++
+	}
+	
+	dest[i] = '\0';
+
+	return (dest);
+}
