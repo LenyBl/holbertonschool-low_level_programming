7bde4acef711b354f0e620a1ac3cc24348d90758
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-10-29 09:59:18 +0000

    fix: remove unnecessary include and simplify capitalization logic in cap_string function

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * cap_string - met en majuscule les premières lettres de chaque mot
  *
@@ -11,17 +10,20 @@ char *cap_string(char *str)
 {
 	int i;
 
-	char separators[] = " \t\n,;.!?\"(){}";
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 
-		if (i == 0 || strchr(separators, str[i - 1]) != NULL)
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - ('a' - 'A');
+
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
-			{
 				str[i] = str[i] - ('a' - 'A');
-			}
 		}
 	}
 
