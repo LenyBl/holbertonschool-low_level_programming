155355809562bad6a1f69bb5118bfd55d575d498
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-03 12:39:49 +0000

    fix: update function documentation to correct name from _print_recursion to _puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_recursion - puts une chaîne de caractères en utilisant récursion
+ * _puts_recursion - puts une chaîne de caractères en utilisant récursion
  *
  * @s: pointeur vers la chaîne de caractères à imprimer
  *
