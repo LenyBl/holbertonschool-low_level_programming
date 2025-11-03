9c9010b5e5b792b91b6ea188a0a5f019b8211d17
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-03 12:43:22 +0000

    feat: add print_rev_recursion function to print a string in reverse using recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev_recursion - imprime une chaîne de caractères en ordre inverse
+ *
+ * @s: pointeur vers la chaîne de caractères à imprimer
+ *
+ * Return: rien
+ */
+void print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	print_rev(*s);
+	print_rev_recursion(*s);
+}
--- recursion/main.h
+++ recursion/main.h
@@ -59,4 +59,6 @@ void set_string(char **s, char *to);
 
 void _puts_recursion(char *s);
 
+void _print_rev_recursion(char *s);
+
 #endif
