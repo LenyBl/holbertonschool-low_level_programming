aa418d235d33f22fcbe1c097bcc1127f19d60b85
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-03 12:36:53 +0000

    feat: add _puts_recursion function to print a string using recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _print_recursion - puts une chaîne de caractères en utilisant récursion
+ *
+ * @s: pointeur vers la chaîne de caractères à imprimer
+ *
+ * Return: rien
+ */
+void _print_recursion(char *s)
+{
+	_puts(s);
+}
