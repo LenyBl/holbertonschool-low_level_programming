16fd41fc7ee338b43e0a5c7e868ed89c1304ab73
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-03 08:34:24 +0000

    fix: correct temporary variable usage in _memcpy function for proper memory copying

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	int temp[n];
+	char temp;
 
 	for (i = 0; i != n; i++)
 	{
-		temp[i] = src[i];
+		temp = src[i];
 		dest[i] = temp[i];
 	}
 	return (dest);
