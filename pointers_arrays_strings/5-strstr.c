81da76aec710c86f70780386c2645d732578df26
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-03 10:02:46 +0000

    feat: add _strstr function to locate the first occurrence of a substring in a string

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - localise la première occurrence de la chaîne 'needle'
+ *             dans la chaîne 'haystack'
+ * @haystack: chaine à chercher
+ * @needle: chaine à trouver
+ *
+ * Return: pointeur vers le début de la première occurrence de 'needle'
+ *         ou NULL si 'needle' n'est pas trouvée
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	char result;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				result = needle[j];
+				return (result);
+			}
+			else
+			{
+				return (0);
+			}
+		}
+	}
+}
