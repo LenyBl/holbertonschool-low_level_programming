5c7a2dfbf9de71182a03907a49c29a308390b997
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-04 08:24:05 +0000

    feat: add _pow_recursion function for exponentiation

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - fonction qui permet de faire x puissance y
+ *
+ * @x: Nombre
+ * @y: Nombre
+ *
+ * Return: retourn x puissance y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	
+	return (_pow_recursion(x, y -1));
+}
