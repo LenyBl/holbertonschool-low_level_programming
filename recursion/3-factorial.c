65e2868fd87f8c434edda19a1b74bf5817fe900a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-04 08:07:04 +0000

    fix: add missing semicolons in factorial function

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,10 +10,10 @@ int factorial(int n)
 {
 	if (n > 0)
 	{
-		return (1 + factorial(n))
+		return (1 + factorial(n));
 	}
 	else
 	{
-		return (-1 + factorial(n))
+		return (-1 + factorial(n));
 	}
 }
