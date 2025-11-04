6e6f6489a8a8227e0eb2f2219cf54603d8adb82f
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-04 11:22:43 +0000

    feat: add is_prime_number function to check for prime numbers

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * is_prime_helper - helps to determine if a number is prime
+ *
+ * @n: number to be checked
+ * @guess: number to divide n by
+ *
+ * Return: 1 if n is divisible by guess, 0 otherwise
+ */
+int is_prime_helper(int n, int guess)
+{
+	if (n % guess == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * is_prime_number - checks if a number is prime
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_helper(n, n - 1));
+	}
+}
