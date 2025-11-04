f873655fd105057b7feccb452b531b9f5212b0ff
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-04 09:00:23 +0000

    fix: update function documentation for _sqrt_recursion and _sqrt_recursion_helper

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <math.h>
 /**
- * _sqrt_recursion - fonction renvoie la racine carrée naturelle d'un nombre
+ * _sqrt_recursion_helper - fonction d'aide pour calculer la racine carrée
  *
  * @n: Nombre
+ * @guess: supposition actuelle
  *
  * Return: La racine carré de n
  */
@@ -15,7 +16,13 @@ int _sqrt_recursion_helper(int n, int guess)
 		return (-1);
 	return (_sqrt_recursion_helper(n, guess + 1));
 }
-
+/**
+ * _sqrt_recursion - fonction renvoie la racine carrée naturelle d'un nombre
+ *
+ * @n: Nombre
+ *
+ * Return: La racine carré de n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
