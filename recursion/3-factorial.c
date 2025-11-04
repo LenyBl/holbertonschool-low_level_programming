0f21ace0077c57409c08e6f2865e6457fca7abd7
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-04 08:06:40 +0000

    feat: implement factorial function to calculate the factorial of a number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - fonction qui retourne le facteur d'un nombre donné
+ *
+ * @n: Nombre
+ *
+ * Return: 0 si n est au dessus de 0 sinon -1
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (1 + factorial(n))
+	}
+	else
+	{
+		return (-1 + factorial(n))
+	}
+}
