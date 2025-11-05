e94c27f5185a59dcf6ea9e712d119496769ee7e9
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-05 08:30:24 +0000

    fix: simplify variable initialization and improve readability in 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,46 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - additionne deux nombres entiers passés en arguments
  *
  * @argc: nombre d'arguments
  * @argv: tableau d'arguments
  *
- * Return: 0
+ * Return: 0 si succès, 1 sinon
  */
 int main(int argc, char *argv[])
 {
-	int i = argc;
+	int i, j, k, sum = 0;
+	char *arg;
 
-	if (i < 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int j = 1;
-	int sum = 0;
-
-	for (j; j < i; j++)
+	for (j = 1; j < argc; j++)
 	{
-		char *arg = argv[j];
-
-		int k;
-
+		arg = argv[j];
 		for (k = 0; arg[k] != '\0'; k++)
 		{
+			if (arg[k] == '-' && k == 0)
+				continue;
 			if (arg[k] < '0' || arg[k] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+		sum += atoi(arg);
 	}
 
-	for (j = 1; j < i; j++)
-	{
-		sum += atoi(argv[j]);
-	}
 	printf("%d\n", sum);
 	return (0);
 }
