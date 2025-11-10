153375a9cd7734ec4956ffeff8224300b2f3fe93
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-10 12:00:03 +0000

    fix: include string.h and correct memory allocation in _strdup function

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
+ * _strdup - duplicates a string by allocating memory
  *
+ * @str: the string to duplicate
  *
- *
- *
- *
+ * Return: a pointer to the duplicated string, or NULL on failure
  */
 char *_strdup(char *str)
 {
@@ -17,12 +16,13 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 
-	cpy = malloc(sizeof(str) * (strlen(str) + 1));
+	cpy = malloc(sizeof(char) * (strlen(str) + 1));
 
 	if (cpy == NULL)
 	{
 		return (NULL);
 	}
 
+	strcpy(cpy, str);
 	return (cpy);
 }
