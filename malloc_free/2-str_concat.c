05f290c504f6809fb6a19ce5b3ff48e6cf9da499
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-10 12:25:38 +0000

    refactor: improve variable naming and formatting in str_concat function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * str_concat - concatenates two strings by allocating memory
  *
+ * @s1: the first string
+ * @s2: the second string
  *
- *
- *
+ * Return: a pointer to the concatenated string, or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *str_concat;
-    int i, j, len1 = 0, len2 = 0;
+	char *concat;
+	int len1 = 0, len2 = 0, i;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    str_concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (str_concat == NULL)
-        return (NULL);
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        str_concat[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		concat[len1 + i] = s2[i];
+	concat[len1 + len2] = '\0';
 
-    for (j = 0; j < len2; j++)
-        str_concat[i + j] = s2[j];
-
-    str_concat[i + j] = '\0';
-
-    return (str_concat);
+	return (concat);
 }
