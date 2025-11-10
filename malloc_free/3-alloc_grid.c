dffaf3b559550292b4f3705c964f94da42af786a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-10 14:11:44 +0000

    fix: update condition to check for valid grid dimensions in alloc_grid function

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,10 +13,14 @@ int **alloc_grid(int width, int height)
 
 	int i, j;
 
-	if (width <= 0 && height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
+
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
