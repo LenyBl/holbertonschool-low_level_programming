14a3fcbff9d8f19a5d4b66389a8e49a9833a47ac
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-10 14:12:59 +0000

    fix: update function documentation for alloc_grid to clarify parameters and return value

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * alloc_grid - fonction qui retourne un pointeur vers un tableau 2D
  *
+ * @width: largeur du tableau
+ * @height: hauteur du tableau
  *
- *
- *
- *
+ * Return: pointeur vers le tableau 2D, NULL en cas d'échec
  */
 int **alloc_grid(int width, int height)
 {
