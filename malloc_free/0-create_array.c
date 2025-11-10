5eac299431689d039b049fb3fb18119710309d27
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-10 09:51:26 +0000

    fix: refactor create_array function for improved readability and structure

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,22 +11,23 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-
-	array = malloc(size * sizeof(char));
+	unsigned int i;
 
 	if (size == 0)
 	{
 		printf("failed to allocate memory\n");
 		return (NULL);
 	}
-	else
-	{
-		unsigned int i;
 
-		for (i = 0; i < size; i++)
-		{
-			array[i] = c;
-		}
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
 	return (array);
 }
