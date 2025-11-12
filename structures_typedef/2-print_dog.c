32e1d6fb4f9a7004eeec0f589804040629ba51ff
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-12 14:15:47 +0000

    feat: implement print_dog function to display dog structure information

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - affiche les informations d'un chien.
+ *
+ * @d: pointeur vers la structure dog à afficher.
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("Dog is NULL\n");
+		return;
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
