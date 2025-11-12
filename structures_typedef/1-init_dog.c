b3849b7dff4b2e9fd73631fd6efaa240c071131e
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-12 14:04:04 +0000

    feat: implement init_dog function to initialize dog structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initialise une structure dog.
+ *
+ * @d: pointeur vers la structure dog à initialiser.
+ * @name: nom du chien.
+ * @age: age du chien.
+ * @owner: nom du proprietaire du chien.
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
