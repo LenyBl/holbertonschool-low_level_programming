50cb5b9dc0bd9854fd546f33976a24ecda441ac8
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-12 08:23:17 +0000

    fix: correct loop condition in free_grid function to properly free allocated memory

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * free_grid - fonction qui libère un tableau 2D précédemment alloué
  *
+ * @grid: pointeur vers le tableau 2D à libérer
+ * @height: hauteur du tableau 2D
  *
- *
- *
- *
- *
+ * Return: rien
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++)
+	for (i = height; i > height; i--)
 	{
-		if (i == height)
-		{
-			free(grid[i]);
-		}
+		free(grid[i]);
 	}
 }
