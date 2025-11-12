312cb5c4ffa1341c90b02927f9d478f8ebb161a9
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-12 14:00:35 +0000

    fix: correct formatting and add missing include guards in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure qui contient des informations sur un chien.
+ *
+ * @name: nom du chien.
+ * @age: age du chien.
+ * @owner: nom du proprietaire du chien.
+ *
+ */
 struct dog
 {
-
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
