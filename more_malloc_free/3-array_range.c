cdd83507cd5494a6bbb9928b1ee21e4b9ae19fc0
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-12 10:09:52 +0000

    feat: implement array_range function to create an array of integers

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - cree un tableau d'entiers contenant toutes les valeurs
+ *  entre min (inclus) et max (exclus)
+ *
+ * @min: la valeur minimale
+ * @max: la valeur maximale
+ *
+ * Return: un pointeur vers le tableau d'entiers, ou NULL en cas d'echec
+ */
+int *array_range(int min, int max)
+{
+	int *grid;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	grid = malloc((max - min + 1) * sizeof(int));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < (max - min + 1); i++)
+	{
+		grid[i] = min + i;
+	}
+
+	return (grid);
+}
