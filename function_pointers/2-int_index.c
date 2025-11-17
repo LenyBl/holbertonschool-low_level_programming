f7e389d59606c89279c366dc0d41270c7f8d9310
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-17 09:23:54 +0000

    feat: implement int_index function to find an integer in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - recherche un entier dans un tableau.
+ *
+ * @array: le tableau d'entiers.
+ * @size: la taille du tableau.
+ * @cmp: pointeur vers la fonction de comparaison.
+ *
+ * Return: l'index du premier élément pour lequel
+ * la fonction de comparaison ne renvoie pas 0.
+ * Si aucun élément ne correspond, retourne -1.
+ * Si la taille est inférieure ou égale à 0, retourne -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+}
