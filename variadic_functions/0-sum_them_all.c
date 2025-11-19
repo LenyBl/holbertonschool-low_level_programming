8c1d4b8dc07eacc6401da8d0b52ac9f828f98087
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-19 08:22:08 +0000

    fix: reorder variable declarations for clarity in sum_them_all function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,13 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	result = 0;
 
+	va_list args;
+	va_start(args, n);
+
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_list args;
-	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
