6824175eb0aeb896bf901bb1d76d3672ff01a816
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-11-19 08:23:42 +0000

    fix: move zero parameter check to the beginning of sum_them_all function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,6 +9,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	unsigned int i;
 	int result;
 
@@ -16,11 +21,6 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_list args;
 	va_start(args, n);
-
-	if (n == 0)
-	{
-		return (0);
-	}
 
 	for (i = 0; i < n; i++)
 	{
