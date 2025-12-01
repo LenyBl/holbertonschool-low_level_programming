dd3b7df7754b13b1c6ec4f44fcb16ecbeb3d76a9
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-01 09:23:51 +0000

    feat: implement add_node function to add a new node at the beginning of a linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ *
+ * @head: double pointer to the head of the list
+ * @str: string to be added to the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	if (str == NULL || *head == NULL)
+		return (-1);
+
+	*head->data = str;
+	*head->next = head;
+
+	*head = head;
+
+	return (*head);
+}
