138f5a8c6f8314e3434dca9a3a4031816ce8161f
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-01 08:58:49 +0000

    feat: implement print_list function to display elements of a singly linked list

---
+++ singly_linked_lists/0-print-list.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	while (h != nullptr)
+	{
+		cout << head->data;
+		if (head->next)
+			cout << "->";
+
+		head = head->next;
+	}
+}
