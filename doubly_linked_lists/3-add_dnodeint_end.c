218dae39bc0de56530e9fe2f8101f365e90c5d31
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-01 14:00:59 +0000

    fix: adjust pointer formatting in add_dnodeint_end function for consistency

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 	{
-		dlistint_t* temp = *head;
+		dlistint_t *temp = *head;
+
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
