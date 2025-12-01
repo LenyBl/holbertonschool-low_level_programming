ec078ff201c14b5b4406533d5ce455851b2a809c
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-01 09:27:17 +0000

    fix: correct implementation of add_node function to properly add a new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,10 +9,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	*head->data = str;
-	*head->next = head;
+	list_t *new_node;
+	unsigned int len = 0;
 
-	*head = head;
+	while (str && str[len])
+		len++;
 
-	return (*head);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
