fad2591831e3b54c52a8c32f5aeb5e70ad516c7f
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-03 13:23:42 +0000

    fix: remove unnecessary blank lines in insert_dnodeint_at_index function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
@@ -47,9 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = new_node;
-
 	current->next = new_node;
 	new_node->prev = current;
-
 	return (new_node);
 }
