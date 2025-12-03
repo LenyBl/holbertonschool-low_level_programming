500c5dada96126c2b38efe309bf7918b249aeb5a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-03 08:45:55 +0000

    fix: remove unnecessary null checks for head and index in get_dnodeint_at_index function

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,9 +11,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (!head || !index)
-		return (NULL);
-
 	while (head != NULL)
 	{
 		if (i == index)
